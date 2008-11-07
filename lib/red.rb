014f1844e85a392fbc84e3747ffaa72a6dfdb491
Jesse Sielaff <jesse.sielaff@gmail.com>
2008-11-06 21:58:53 -0500

    Updated to version 4.1.5.

--- lib/red.rb
+++ lib/red.rb
@@ -15,7 +15,7 @@ require 'red/nodes/logic_nodes'
 require 'red/nodes/variable_nodes'
 
 module Red # :nodoc:
-  VERSION = '4.1.4'
+  VERSION = '4.1.5'
   
   ARRAY_NODES = {
     :and          => LogicNode::Conjunction::And,
